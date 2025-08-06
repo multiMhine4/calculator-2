34fc37d1c5a92b53c8242e9ca80918794b456956
Timothy Tesluk <timothy.tesluk@gmail.com>
2025-08-06 14:28:51 +0800

    Create display manager object

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const displayDiv = document.querySelector("#display");
+
+const displayManager = {
+    // Adds thousands separator to numbers (2000 to 2,000)
+    format: function(numStr) {
+        let list = numStr.split("");
+        let index = numStr.length;
+        while (index-3 > 0) {
+            list.splice(index-3, 0, ",");
+            index -= 3;
+        }
+        return list.join("");
+    },
+
+    show: function(numStr) {
+        displayDiv.innerText = this.format(numStr);
+    },
+
+    clear: function() {
+        displayDiv.innerText = "";
+    }
+}
+
 const buttons = document.querySelectorAll(".button");
 
 buttons.forEach((button) => {
